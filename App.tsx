63d55af57da12ebea9872ca4f6e56dd541c31160
Deise Domingues <dominguesdeise@gmail.com>
2024-11-09 20:44:38 -0300

    feat: adiciona pressable para exibir "Ver mais gatinhos"

--- App.tsx
+++ App.tsx
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  View
+  } from 'react-native';
 
-export default function App() {
+  export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
+  <View style={styles.container}>
+
+    <Pressable
+    style={styles.button}>
+      <Text
+        style={styles.buttonText}>
+          Ver mais gatinhos
+      </Text>
+    </Pressable>
+
+  </View>
+  );
+  }
+
+
+  const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  flex: 1,
+  backgroundColor: '#fff',
+  alignItems: 'center',
+  justifyContent: 'center',
   },
-});
+
+  button: {
+    width: '10%',
+    backgroundColor: '#0096F3', //material design blue 500
+    padding: 12,
+    borderRadius: 4
+  },
+
+  buttonText: {
+    color: 'white',
+    textAlign: 'center'
+  }
+
+  });
+  
