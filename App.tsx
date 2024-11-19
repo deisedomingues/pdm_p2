ff45851c72e055340d6a8119876484886bee0013
Deise Domingues <dominguesdeise@gmail.com>
2024-11-19 15:33:56 -0300

    feat: adiciona useState e configura importações

--- App.tsx
+++ App.tsx
@@ -1,24 +1,37 @@
 import {
+  FlatList,
   Pressable,
   StyleSheet,
   Text,
   View
   } from 'react-native';
 
+import {
+  useState
+  } from 'react';
+
+import {
+  CAT_API
+  } from 'react-native-dotenv';
+
+  
   export default function App() {
+    const { CAT_API } = process.env;
+    const [gatinho, setGatinho] = useState<Gatos[]>([]);
+
   return (
 
-  <View style={styles.container}>
+    <View style={styles.container}>
 
-    <Pressable
-    style={styles.button}>
-      <Text
-        style={styles.buttonText}>
-          Ver mais gatinhos
-      </Text>
-    </Pressable>
-
-  </View>
+      <Pressable
+      style={styles.button}>
+        <Text
+          style={styles.buttonText}>
+            Ver mais gatinhos
+        </Text>
+      </Pressable>
+      
+    </View>
   );
   }
 
