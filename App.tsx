a330ebcea3b1ee3cf02d728aaa73871c636565c0
Deise Domingues <dominguesdeise@gmail.com>
2024-11-20 20:04:12 -0300

    feat: adiciona imagem de gato

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import fotoGatinho from './assets/gato.png';
+
 import {
   useState
 } from 'react';
@@ -11,27 +13,41 @@ import {
   View
 } from 'react-native';
 
+
+
 export default function App() {
   const [gatinho, setGatinho] = useState([]);
 
-  
   const fetchGatos = async () => {
     const response = await fetch('https://api.thecatapi.com/v1/images/search?limit=5');
-    const data = await response.json();
-    setGatinho(prevGatinhos => [...prevGatinhos, ...data]);
-  };  
+    const novosGatinhos = await response.json();
+    setGatinho(antigosGatinhos => [...antigosGatinhos, ...novosGatinhos]);
+    
+  };
 
-  
-  const handlePress = () => {
+  const buscarGatinhos = () => {
     fetchGatos();  
   };
 
   return (
     <View style={styles.container}>
 
-      <Pressable style={styles.button} onPress={handlePress}>
+      <Image source={fotoGatinho} style={styles.gatinhoImagem} />
+
+      <Pressable style={styles.button} onPress={buscarGatinhos}>
         <Text style={styles.buttonText}>Ver mais gatinhos</Text>
       </Pressable>
+
+      <FlatList
+        data={gatinho}
+        keyExtractor={(item) => item.id.toString()} 
+        renderItem={({ item }) => (
+          <Image
+            source={{ uri: item.url }} 
+            style={styles.gatinhosLista}
+          />
+        )}
+      />
      
     </View>
   );
@@ -46,20 +62,27 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   button: {
-    width: '80%',
-    backgroundColor: '#0096F3', // Material design blue 500
+    width: '20%',
+    backgroundColor: '#8e2b1a',
     padding: 12,
-    borderRadius: 4,
-    marginBottom: 20,
+    borderRadius: 50,
+    marginBottom: 10,
   },
   buttonText: {
     color: 'white',
     textAlign: 'center',
   },
-  image: {
+  gatinhosLista: {
     width: 300,  
     height: 200,
     marginBottom: 10,
-    borderRadius: 8,
+    borderRadius: 5,
   },
+  gatinhoImagem:{
+    width: 320,
+    padding: 12,
+    marginBottom: 10,
+    borderRadius: 5,
+  }
+
 });
